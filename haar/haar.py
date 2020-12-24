2157b5f3d6e61d734783847a8dc91ce802078438
guzel <sharafutdinova@technokratos.com>
2020-12-24 17:46:17 +0300

    added haar

---
+++ haar/haar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+import cv2 as cv
+
+
+green = (0, 255, 0)
+face_cascade = cv.CascadeClassifier(os.path.abspath('/Users/guzelsarafutdinova/Desktop/haarcascade_frontalface_alt.xml'))
+for file in os.listdir('/Users/guzelsarafutdinova/Desktop'):
+    file_name, file_extension = os.path.splitext(file)
+    if file_extension == '.jpg':
+        image = cv.imread('/Users/guzelsarafutdinova/Desktop/' + file)
+        faces = face_cascade.detectMultiScale(image)
+        for (x, y, width, height) in faces:
+            x2, y2 = x + width, y + height
+            cv.rectangle(image, (x, y), (x2, y2), green, 2)
+        cv.imshow("Face", image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
